8ac63e6c150b929c42e9414551ff40d03a0c2e4c
neoxu <neoxuisme@gmail.com>
2014-12-11 17:10:01 +0800

    version 1.0.0

---
+++ Assets/Resources/Scripts/GameStart.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+	public const string URL = "http://127.0.0.1:3100/";
+	public string UserName = "username";
+	public string Password = "password";
+
+	private void SendLogin(string account, string password) {
+		WWWForm form = new WWWForm();
+		form.AddField("user", UserName);
+		form.AddField("password", Password);
+		WWW www = new WWW(URL+"login", form.data);
+		StartCoroutine(WaitLogin(www));
+	}
+
+	IEnumerator WaitLogin(WWW www)
+	{
+		while (!www.isDone)
+			yield return null;  
+		
+		if (string.IsNullOrEmpty(www.error))
+		{
+			Debug.Log(www.text);
+		} else
+			Debug.Log(www.error);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GSocket.Get.ReceivePotocol();
+    }
+    
+    void OnGUI() {
+		GUI.Label(new Rect(220, 100, 100, 40), "UserName");
+		UserName = GUI.TextArea(new Rect(300, 100, 100, 40), UserName, 255);
+		GUI.Label(new Rect(220, 150, 100, 40), "Password");
+		Password = GUI.TextArea(new Rect(300, 150, 100, 40), Password, 255);
+
+		if (GUI.Button(new Rect(100, 100, 100, 100), "Http Login")) {
+			SendLogin(UserName, Password);
+		}
+
+		if (GUI.Button(new Rect(100, 300, 200, 60), "Socket.io Connect")) {
+			GSocket.Get.Connect();
+		}
+	}
+}
